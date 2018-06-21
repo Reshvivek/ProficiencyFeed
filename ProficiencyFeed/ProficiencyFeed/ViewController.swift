178c539f0d889cacfadf70241394bba8d6e352aa
Reshvivek <resh.ohm@gmail.com>
2018-06-21 12:33:39 +1000

    App basic files

---
+++ ProficiencyFeed/ProficiencyFeed/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ProficiencyFeed
+//
+//  Created by vivek venudasan on 21/6/18.
+//  Copyright © 2018 reshma. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UICollectionViewController,UICollectionViewDelegateFlowLayout {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
